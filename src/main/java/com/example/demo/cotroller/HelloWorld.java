90ffca492d43ef69c4a987950a7a2e6c9f0c2440
shawn <237195092@qq.com>
2018-08-22 22:52:14 +0800

    增加jpa查询

--- src/main/java/com/example/demo/cotroller/HelloWorld.java
+++ src/main/java/com/example/demo/cotroller/HelloWorld.java
@@ -1,13 +1,24 @@
 package com.example.demo.cotroller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dao.PersonDAO;
+import com.example.demo.model.Person;
 
 @RestController
 @RequestMapping("/test")
 public class HelloWorld {
-    @RequestMapping("hello")
+    @Autowired
+    private PersonDAO personDAO;
+    
+	@RequestMapping("hello")
     public String hello() {
-        return "hello world test";
+		List<Person> list = personDAO.findAll();
+		Person p = list.get(0);
+		return list.size() + p.getName() + p.getSex();
     }
 }
