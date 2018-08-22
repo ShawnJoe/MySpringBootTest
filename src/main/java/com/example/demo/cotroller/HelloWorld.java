b0c525cd4f90144fea984c1581589757705ebcda
ZhaoXu <0223000318@zte.com.cn>
2018-08-22 16:51:01 +0800

    MySpringBootTest

---
+++ src/main/java/com/example/demo/cotroller/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.demo.cotroller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class HelloWorld {
+    @RequestMapping("hello")
+    public String hello() {
+        return "hello world";
+    }
+}
